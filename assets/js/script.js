bda6b85acfa90b740cbfe740a1de45ec9bced2df
sinastiller <sinastiller@gmx.de>
2022-10-16 12:55:58 +0000

    Add playerMovesLeft to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,6 @@
 
 const section = document.getElementById("section");
 const playerMovesLeft = document.getElementById("playerMovesLeft");
-const playerMoves = 5;
+const playerMoves = 8;
 
 playerMovesLeft.textContent = playerMoves;
